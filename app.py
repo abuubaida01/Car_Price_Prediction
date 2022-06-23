8b26292c6ae889e864439097c2176b49fd52a7c3
abuubaida01 <abuubaida901@gmail.com>
2022-06-23 23:51:12 +0500

    will Deploly

--- app.py
+++ app.py
@@ -1,109 +1,142 @@
+from statistics import mode
+from xgboost import XGBRegressor
+import xgboost
 import numpy as np
 import pandas as pd
 import pickle
+import sklearn
 from flask import Flask, render_template, request
 # request for getting dat from Form
 
-df = pickle.load(open('dataframe.pkl','rb'))
-pipe = pickle.load(open('pipe.pkl', 'rb')) # I have loaded DataFrame
-# pipe = pickle.load(open('pipe.pkl', 'rb'))  # I have loaded model
+# df = pickle.load(open('dataframe.pkl','rb'))
+df = pd.read_csv('dataframe.csv')
+pipe = pickle.load(open('pipe.pkl', 'rb'))  # I have loaded DataFrame
+
+# #___________________________ model ___________________#
+# from sklearn.preprocessing import QuantileTransformer, RobustScaler
+# from xgboost import XGBRegressor
+# from sklearn.compose import  ColumnTransformer
+# from sklearn.preprocessing import OneHotEncoder, LabelEncoder, OrdinalEncoder
+# from sklearn.pipeline import make_pipeline
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestRegressor
+# X = df.copy()
+# y = pd.read_csv('label.csv')
+# # X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+
+
+# transformer = ColumnTransformer(transformers=[
+#     ('scaler1',QuantileTransformer(),['Mileage', 'Engine Capacity', 'Model Year']),
+#     ('Ordinal1', OrdinalEncoder(categories=[['Hatchback','Sedan','Suv','Mini Van','Crossover','Van']]),['Body Type']),
+#     ('Ordinal2', OrdinalEncoder(categories=[['Petrol','Diesel','Hybrid']]), ['Engine Type']),
+#     ('Ordinal3', OrdinalEncoder(categories=[['Automatic', 'Manual']]), ['Transmission']),
+#     ('ohe1', (OneHotEncoder(drop='first', sparse=False)),['Model','variant','Color','Manufacturer', 'Assembly'])   
+# ], remainder='passthrough')
+
+# pipe_r = make_pipeline(transformer, RandomForestRegressor())
+# pipe_r.fit(X_train,y_train)
+
+# pipe_r.predict(X_test)
+
+
 
 app = Flask(__name__)  # important
-
 
 @app.route('/')  # for pointing homepage
 def index():
 
-    
-    # Company = sorted(df['Company'].unique())
-    # TypeName = sorted(df['TypeName'].unique())
-    # Inches = sorted(df['Inches'].unique())
-    # Ram = sorted(df['Ram'].unique())
-    # OpSys = sorted(df['OpSys'].unique())
-    # Weight = sorted(df['Weight'].unique())
-    # IPS = sorted(df['IPS'].unique())
-    # touch_screen = sorted(df['touch_screen'].unique())
-    # PPI = sorted(df['PPI'].unique())
-    # Brand = sorted(df['Brand'].unique())
-    # SDD = sorted(df['SDD'].unique())
-    # HDD = sorted(df['HDD'].unique())
-    # GPU = df['GPU'].unique()
+    model_year = sorted(df['Model Year'].unique())
+    mileage = sorted(df['Mileage'].unique())
+    engine_type = sorted(df['Engine Type'].unique())
+    engine_capacity = sorted(df['Engine Capacity'].unique())
+    transmission = sorted(df['Transmission'].unique())
+    color = sorted(df['Color'].unique())
+    Assembly = sorted(df['Assembly'].unique())
+    body_type = sorted(df['Body Type'].unique())
+    manufacturer = sorted(df['Manufacturer'].unique())
+    model = df['Model'].unique()
+    variant = df['variant'].unique()
+    abs = df['ABS'].unique()
+    fm_radio = df['AM/FM Radio'].unique()
+    air_bags = df['Air Bags'].unique()
+    air_cond = df['Air Conditioning'].unique()
+    allay_rim = df['Alloy Rims'].unique()
+    cd_player = df['CD Player'].unique()
+    cassette_player = df['Cassette Player'].unique()
+    climate_control = df['Climate Control'].unique()
+    coolbox = df['CoolBox'].unique()
+    cruise_control = df['Cruise Control'].unique()
+    dvd_player = df['DVD Player'].unique()
+    front_camera = df['Front Camera'].unique()
+    front_speakers = df['Front Speakers'].unique()
+    heated_seats = df['Heated Seats'].unique()
+    immobilizer_key = df['Immobilizer Key'].unique()
+    keyless_entry = df['Keyless Entry'].unique()
+    navigation_system = df['Navigation System'].unique()
+    power_locks = df['Power Locks'].unique()
+    power_mirrors = df['Power Mirrors'].unique()
+    power_steering = df['Power Steering'].unique()
+    power_windows = df['Power Windows'].unique()
+    rear_ac = df['Rear AC Vents'].unique()
+    rear_camera = df['Rear Camera'].unique()
+    rear_seat = df['Rear Seat Entertainment'].unique()
+    rear_speakers = df['Rear speakers'].unique()
+    steering_switches = df['Steering Switches'].unique()
+    sun_roof = df['Sun Roof'].unique()
+    usb = df['USB and Auxillary Cable'].unique()
 
-    return render_template('index.html', company=Company, typename=TypeName, inches=Inches, ram=Ram, opsys=OpSys, weight=Weight, ips=IPS, touchscreen=touch_screen, ppi=PPI, brand=Brand, sdd=SDD, hdd=HDD, gpu=GPU)
+    return render_template('index.html', model_year=model_year, mileage=mileage, engine_type=engine_type, engine_capacity=engine_capacity, transmission=transmission, color=color, Assembly=Assembly, body_type=body_type, manufacturer=manufacturer, model=model, variant=variant, abs=abs, fm_radio=fm_radio, air_bags=air_bags, air_cond=air_cond, allay_rim=allay_rim, cd_player=cd_player, cassette_player=cassette_player, climate_control=climate_control, coolbox=coolbox, cruise_control=cruise_control, dvd_player=dvd_player, front_camera=front_camera, front_speakers=front_speakers, heated_seats=heated_seats, immobilizer_key=immobilizer_key, keyless_entry=keyless_entry, navigation_system=navigation_system, power_locks=power_locks, power_mirrors=power_mirrors, power_steering=power_steering, power_windows=power_windows, rear_ac=rear_ac, rear_camera=rear_camera, rear_seat=rear_seat, rear_speakers=rear_speakers, steering_switches=steering_switches, sun_roof=sun_roof, usb=usb)
 
 
-@app.route('/predict', methods=['post','get'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    Company = request.form.get('company')
-    TypeName = request.form.get('typename')
-    Inches = request.form.get('inches')
-    Ram = request.form.get('ram')
-    OpSys = request.form.get('opsys')
-    Weight = request.form.get('weight')
-    IPS = request.form.get('ips')
-    touch_screen = request.form.get('touchscreen')
-    PPI = request.form.get('ppi')
-    Brand = request.form.get('brand')
-    SDD = request.form.get('sdd')
-    HDD = request.form.get('hdd')
-    GPU = request.form.get('gpu')
+    model_year = request.form.get('model_year')
+    mileage = request.form.get('mileage')
+    engine_type = request.form.get('engine_type')
+    engine_capacity = request.form.get('engine_capacity')
+    transmission = request.form.get('transmission')
+    color = request.form.get('color')
+    Assembly = request.form.get('Assembly')
+    body_type = request.form.get('body_type')
+    manufacturer = request.form.get('manufacturer')
+    model = request.form.get('model')
+    variant = request.form.get('variant')
+    abs = request.form.get('abs')
+    fm_radio = request.form.get('fm_radio')
+    air_bags = request.form.get('air_bags')
+    air_cond = request.form.get('air_cond')
+    allay_rim = request.form.get('allay_rim')
+    cd_player = request.form.get('cd_player')
+    cassette_player = request.form.get('cassette_player')
+    climate_control = request.form.get('climate_control')
+    coolbox = request.form.get('coolbox')
+    cruise_control = request.form.get('cruise_control')
+    dvd_player = request.form.get('dvd_player')
+    front_camera = request.form.get('front_camera')
+    front_speakers = request.form.get('front_speakers')
+    heated_seats = request.form.get('heated_seats')
+    immobilizer_key = request.form.get('immobilizer_key')
+    keyless_entry = request.form.get('keyless_entry')
+    navigation_system = request.form.get('navigation_system')
+    power_locks = request.form.get('power_locks')
+    power_mirrors = request.form.get('power_mirrors')
+    power_steering = request.form.get('power_steering')
+    power_windows = request.form.get('power_windows')
+    rear_ac = request.form.get('rear_ac')
+    rear_camera = request.form.get('rear_camera')
+    rear_seat = request.form.get('rear_seat')
+    rear_speakers = request.form.get('rear_speakers')
+    steering_switches = request.form.get('steering_switches')
+    sun_roof = request.form.get('sun_roof')
+    usb = request.form.get('usb')
 
-# let's make and Dataframe for prediction as our model takes just dataframe 
-    
-    
-    prediction = pipe.predict(input)[0] # it will give list which 0 element would be our prediction
+    input = pd.DataFrame([[model_year, mileage, engine_type, engine_capacity, transmission, color, Assembly, body_type, manufacturer, model, variant, abs, fm_radio, air_bags, air_cond, allay_rim, cd_player, cassette_player, climate_control, coolbox, cruise_control,dvd_player, front_camera, front_speakers, heated_seats, immobilizer_key, keyless_entry, navigation_system, power_locks, power_mirrors, power_steering, power_windows, rear_ac, rear_camera, rear_seat, rear_speakers, steering_switches, sun_roof, usb]])
 
-
+    prediction = pipe.predict(input.to_xarray())
     return str(prediction)
+    # return str(prediction)/
+    # return render_template('index.html', prediction_text=f'Predicted Price is {prediction}')
 
-    # array.append(Inches = request.form.get('Inches'))
-    # array.append(TypeName = request.form.get('TypeName'))
-    # array.append(Ram = request.form.get('Ram'))
-    # array.append(OpSys = request.form.get('OpSys'))
-    # array.append(Weight = request.form.get('Weight'))
-    # array.append(IPS = request.form.get('IPS'))
-    # array.append(touch_screen = request.form.get('touch_screen'))
-    # array.append(PPI = request.form.get('PPI'))
-    # array.append(Brand = request.form.get('Brand'))
-    # array.append(SDD = request.form.get('SDD'))
-    # array.append(HDD = request.form.get('HDD'))
-    # array.append(GPU = request.form.get('GPU'))
-
-    # now here I will send each columns of DF as a list to the index.html
-
-
-# @app.route('/recommend')
-# def recommend_ui():
-#     return render_template('recommend.html')
-
-# @app.route('/recommend_books', methods =['post']) ## for recommending books method is post, cause we are getting data from user Forms
-
-# def recommend():
-#     input_data = request.form.get('user_input').strip()  ## got title from user
-#     # now I will show result
-
-#     index = np.where(pt.index == input_data)[0][0]
-#     if  index >=0 :
-#         similar_item=sorted(list(enumerate(cs[index])), key=lambda x: x[1], reverse=True)[1:9]
-
-#         data= []
-#         book['Book-Title'] = book['Book-Title'].drop_duplicates() # removing duplicates
-
-#         for i in similar_item:
-#             # print(i) # we got index, need index[i[0]]
-#             ## now I will match this title with each title of Books and will fetch book author name and Image
-#             # print(pt.index[i[0]])
-#             item= []
-#             # here book must bee chosen by top-50 books so that he/she can see recommendation
-
-#             item.extend(list(book[book['Book-Title']==pt.index[i[0]]]['Book-Title'].values))
-#             item.extend(list(book[book['Book-Title']==pt.index[i[0]]]['Book-Author'].values))
-#             item.extend(list(book[book['Book-Title']==pt.index[i[0]]]['Image-URL-M'].values))
-#             data.append(item)  ## we will get the list of list
-#         # print(data)
-
-#         return render_template('recommend.html', data = data) #passing to recommend.html
-#     else:
-#         return "Book is out of stock"
 if __name__ == "__main__":
     app.run(debug=True)
